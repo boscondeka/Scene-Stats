d6f0f6c47795823ca4f0e1fe8aab569eb89aa525
Davidwekesaa <dweke12@gmail.com>
2024-10-26 16:38:54 +0300

    latest updates

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("caff94aab9898f98")
+/******/ 		__webpack_require__.h = () => ("cf84a5da5a7e14f2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1cccdc7270d7acbe")
+/******/ 		__webpack_require__.h = () => ("dcd2410d7e344fad")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
